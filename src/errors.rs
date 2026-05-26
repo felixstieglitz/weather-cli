42b57d974652b1f5b06958150aaeb2bb3d902aa5
Felix Stieglitz <stieglitzfelix@icloud.com>
2026-05-26 22:50:04 +0200

    implement From for AppError

--- src/errors.rs
+++ src/errors.rs
@@ -3,13 +3,18 @@ use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub enum AppError {
-    HttpError,
+    HttpError(reqwest::Error),
     CoordinateError,
     InputError,
 }
 impl Display for AppError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+impl From<reqwest::Error> for AppError {
+    fn from(e: reqwest::Error) -> Self {
+        AppError::HttpError(e)
     }
 }
 impl Error for AppError {}
